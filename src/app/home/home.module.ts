378e324dd0e4cfce25876c0d386644151a234990
khai <kaitokido14120@gmail.com>
2018-05-28 20:17:27 +0700

    version1.0: CauHinhRoutes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TrangChuComponent } from './trang-chu/trang-chu.component';
 import { TrangChiTietSanPhamComponent } from './trang-chi-tiet-san-pham/trang-chi-tiet-san-pham.component';
 import { GioHangComponent } from './gio-hang/gio-hang.component';
-
+import { Routes, RouterModule } from '@angular/router';
+const homeRouting: Routes = [
+  //localhost:4200 hoặc localhost/home thì sẽ vào component nào
+  {path:'',component:TrangChuComponent},
+];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,RouterModule.forChild(homeRouting)
   ],
   declarations: [TrangChuComponent, TrangChiTietSanPhamComponent, GioHangComponent]
 })
